785a90de4ecd68c85c61856e2c9c315c16ff3fc6
Guy Michael <guy@engieapp.com>
2020-05-06 03:02:11 +0300

    fixed TextItem props type

--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/items/TextItem.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/items/TextItem.kt
@@ -3,20 +3,23 @@ package com.guymichael.reactdroid.extensions.components.list.items
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.IdRes
-import com.guymichael.kotlinreact.model.props.CharSequenceProps
 import com.guymichael.reactdroid.core.model.ASimpleComponent
+import com.guymichael.reactdroid.extensions.components.list.model.DataItemProps
 import com.guymichael.reactdroid.extensions.components.text.renderText
 import com.guymichael.reactdroid.extensions.components.text.withText
 
-class TextItem(v: View, textView: TextView) : ASimpleComponent<CharSequenceProps>(v) {
+class TextItem(v: View, textView: TextView) : ASimpleComponent<DataItemProps<CharSequence>>(v) {
 
     private val cTxt = withText(textView)
 
     constructor(v: View, @IdRes textViewRes: Int)
     : this(v, v.findViewById<TextView>(textViewRes))
 
+    @Throws(ClassCastException::class)
+    constructor(v: View)
+    : this(v, v as TextView)
 
     override fun render() {
-        cTxt.renderText(props.value)
+        cTxt.renderText(props.data)
     }
 }
