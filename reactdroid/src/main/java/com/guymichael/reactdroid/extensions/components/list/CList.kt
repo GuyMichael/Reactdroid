369fc3867caf99da98fb7afc671e76bb623da64e
Guy Michael <guy@engieapp.com>
2020-05-07 20:20:23 +0300

    CList scroll experiments

--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/CList.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/CList.kt
@@ -18,41 +18,75 @@ class CList(
 
     @JvmOverloads
     constructor(v: RecyclerView, orientation: Int = RecyclerView.VERTICAL)
-        : this(v, createAdapter(v, orientation))
+    : this(v, createAdapter(v, orientation))
+
+
+    /* lifecycle */
 
     override fun createInitialState(props: ListProps) = ListState(props.uncontrolled_initialScrollIndex)
 
     override fun componentDidMount() {
         //if we received scroll position through props (initial or controlled),
         if (getScrollIndex() != null) {
-            // listen on scroll state changes to update state's scrollIndex
-            adapter.addOnListScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                        onListScrollIndexChanged(adapter.getActualFirstVisiblePosition())
-                    }
-                }
-            })
+            // listen on scroll state changes to update state's scrollIndex (align with actual - adapter)
+            listenOnScrollStateChanges()
+        }
+    }
+
+    override fun componentDidUpdate(prevProps: ListProps, prevState: ListState, snapshot: Any?) {
+        // "listen" on data-size changes to update state's scrollIndex (align with actual - adapter)
+        // (scrollListener isn't invoked when data changes, sadly)
+        if (prevProps.items.size != this.props.items.size) {
+            alignOwnStateScrollWithActualIfNeeded()
         }
     }
 
 
 
 
-
-
-    private fun getScrollIndex(): Int? {
-        //controlled scroll mode
-        return this.props.controlledScroll?.first
-
-        //or uncontrolled
-        ?: this.ownState.uncontrolledScrollIndex
+    /* API */
+    fun onRender(items: List<ListItemProps>) {
+        onRender(ListProps(items))
     }
 
 
 
+
+    /* privates */
+
+    private fun getScrollIndex(props: ListProps = this.props, state: ListState = this.ownState): Int? {
+        //controlled scroll mode
+        return props.controlledScroll?.first
+
+        //or uncontrolled
+        ?: state.uncontrolledScrollIndex
+    }
+
+    private fun listenOnScrollStateChanges() {
+        adapter.addOnListScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    onListScrollIndexChanged(adapter.getActualFirstVisiblePosition())
+                }
+            }
+        })
+    }
+
+    private fun alignOwnStateScrollWithActualIfNeeded() {
+        getScrollIndex()?.takeIf { adapter.itemCount > 0 }?.also { stateIndex ->
+        adapter.getActualFirstVisiblePosition().takeIf { it >= 0 && it != stateIndex }
+            ?.let { actualIndex ->
+
+            setState(ownState.cloneWithNewScroll(actualIndex))
+        }}
+    }
+
+
+
+    /* callbacks */
+
     //called only when props provide scroll index (controlled or not)
-    fun onListScrollIndexChanged(newIndex: Int) {
+    private fun onListScrollIndexChanged(newIndex: Int) {
         //controlled scroll mode
         this.props.controlledScroll?.second?.also {
             it.invoke(newIndex)
@@ -64,20 +98,23 @@ class CList(
 
 
 
-    fun onRender(items: List<ListItemProps>) {
-        onRender(ListProps(items))
+
+
+    /* render */
+
+    private fun renderAdapterScrollPosition(index: Int, smoothScroll: Boolean) {
+        if (smoothScroll) {
+            adapter.smoothScroll(index)
+        } else {
+            adapter.scrollImmediately(index)
+        }
     }
 
-
-
-
-    private var didFirstRenderScrollPosition = false
+    private var didFirstRenderScrollPosition = false //for first scroll (immediate) or latter (smooth)
     private fun renderScrollPosition(scrollIndex: Int) {
-        if (adapter.itemCount > 0 && adapter.getActualFirstVisiblePosition() != scrollIndex) {
-            if (didFirstRenderScrollPosition) {
-                adapter.smoothScroll(scrollIndex)
-            } else {
-                adapter.scrollImmediately(scrollIndex)
+        if (adapter.itemCount > 0) {
+            if (adapter.getActualFirstVisiblePosition() != scrollIndex) {
+                renderAdapterScrollPosition(scrollIndex, didFirstRenderScrollPosition)
             }
 
             //this is considered the first scroll-render with non-empty list
@@ -88,10 +125,12 @@ class CList(
     private var didFirstRender = false
     override fun render() {
         if( !didFirstRender || adapter.getAllItems() != props.items) { //THINK efficiency
+            //notify data set changed
             super.render()
         }
 
-        getScrollIndex()?.let(::renderScrollPosition)
+        //update adapter's scroll position
+        getScrollIndex()?.also(::renderScrollPosition)
 
         didFirstRender = true
     }
--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/model/ListState.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/model/ListState.kt
@@ -7,5 +7,11 @@ data class ListState(
         val uncontrolledScrollIndex: Int? = null //doesn't cause re-renders
     ) : OwnState() {
 
+    fun cloneWithNewScroll(uncontrolledScrollIndex: Int): ListState {
+        return ListState(
+            uncontrolledScrollIndex = uncontrolledScrollIndex
+        )
+    }
+
     override fun getAllMembers() = emptyList<Any?>()
 }
