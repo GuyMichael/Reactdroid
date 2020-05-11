68b6781a176d3df7e2ce9534eb75f40a78342378
Guy Michael <guy@engieapp.com>
2020-05-11 22:02:14 +0300

    added WithPermissions component

---
+++ reactdroid/src/main/java/com/guymichael/reactdroid/core/permissions/PermissionProps.kt
@@ -0,0 +1,36 @@
+package com.guymichael.reactdroid.core.permissions
+
+import com.guymichael.kotlinreact.model.OwnProps
+import com.guymichael.reactdroid.core.Utils
+
+data class PermissionProps<COMPONENT_PROPS : OwnProps>(
+        /** Requested permissions to grant. Until ALL are granted, the inner component will
+         * be 'GONE' - never rendered.
+         * Permissions may be changed later on, in which case same logic applies, except that
+         * the inner component may already be rendered from previous permissions grant,
+         * so keep that in mind (but it WILL become 'GONE' if new permissions aren't granted yet) */
+        val permissions: List<String>,
+
+        /** props for your (inner) component */
+        val componentProps: COMPONENT_PROPS,
+
+        //doesn't affect re-renders
+        /** Action to execute if user denied the requested permissions, normally close the page.
+         * Doesn't affect re-renders
+         * @see `PermissionsDeniedException` regarding the [Throwable] argument `e`, which *might*,
+         * and probably will be, of this type*/
+        val initial_actionOnDenied: (e: Throwable, WithPermissions<COMPONENT_PROPS, *, *>) -> Unit
+            = { _, c -> Utils.getActivity(c.mView)?.onBackPressed() },
+
+        /** If `true`, and user has `denied always` at least one of the given permissions,
+         * the app settings will open to let the user change there.
+         * If the user grants permissions, and there are some permissions still missing
+         * (weren't on 'always deny'), they will be automatically requested when app resumes.
+         * Doesn't affect re-renders*/
+        val initial_goToAppSettingsIfAlwaysDeny: Boolean = false
+    ) : OwnProps() {
+
+    override fun getAllMembers() = listOf(
+        componentProps, permissions
+    )
+}
---
+++ reactdroid/src/main/java/com/guymichael/reactdroid/core/permissions/WithPermissions.kt
@@ -0,0 +1,77 @@
+package com.guymichael.reactdroid.core.permissions
+
+import android.view.View
+import com.guymichael.kotlinreact.model.OwnProps
+import com.guymichael.kotlinreact.model.ownstate.BooleanState
+import com.guymichael.reactdroid.core.Utils
+import com.guymichael.reactdroid.core.activity.ComponentActivity
+import com.guymichael.reactdroid.core.execute
+import com.guymichael.reactdroid.core.model.AComponent
+import com.guymichael.reactdroid.core.model.AHOC
+import com.guymichael.reactdroid.core.renderOrGone
+
+class WithPermissions<COMPONENT_PROPS : OwnProps, V : View, C : AComponent<COMPONENT_PROPS, *, V>>(
+        component: C
+    ) : AHOC<PermissionProps<COMPONENT_PROPS>, COMPONENT_PROPS, V, C, BooleanState>(
+        component, false
+    ) {
+
+    override fun createInitialState(props: PermissionProps<COMPONENT_PROPS>) = BooleanState(checkPermissions(props))
+    override fun mapToComponentProps(hocProps: PermissionProps<COMPONENT_PROPS>) = hocProps.componentProps
+
+    override fun componentDidMount() {
+        if( !ownState.value) {
+            //permissions not granted according to state
+            PermissionsLogic.requestPermissions(
+                    Utils.getActivity(mView.context, ComponentActivity::class.java)!!
+                    , props.permissions
+                    , requestThroughSettingsIfAlwaysDeny = true
+                )
+                .then {
+                    setState(BooleanState(true))
+                }
+                .catch { e ->
+                    //e is (probably) a PermissionsDeniedException
+                    props.initial_actionOnDenied.invoke(e, this)
+                }
+                .execute(this)
+        }
+    }
+
+    override fun componentDidUpdate(prevProps: PermissionProps<COMPONENT_PROPS>
+                                    , prevState: BooleanState, snapshot: Any?) {
+
+        //update state if requested permissions changed
+        if (this.props.permissions != prevProps.permissions) {
+            this.ownState.takeIf {
+                it.value != checkPermissions(this.props)
+            }?.also {
+                setState(BooleanState(!it.value))
+            }
+        }
+    }
+
+
+
+
+
+    private fun checkPermissions(props: PermissionProps<COMPONENT_PROPS> = this.props): Boolean {
+        return PermissionsLogic.checkPermissions(mView.context, props.permissions.toTypedArray())
+    }
+
+
+
+
+    override fun render() {
+        mComponent.renderOrGone(if (ownState.value) props.componentProps else null)
+    }
+}
+
+
+
+
+
+fun <COMPONENT_PROPS : OwnProps, V : View, C : AComponent<COMPONENT_PROPS, *, V>>
+withPermissions(component: C) = WithPermissions(
+    component
+)
