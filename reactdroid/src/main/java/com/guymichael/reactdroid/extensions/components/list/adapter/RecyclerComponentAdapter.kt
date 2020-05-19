da74f25f202d383af8014aabb74c69efcdd9dbf8
Guy Michael <guy@engieapp.com>
2020-05-19 23:28:09 +0300

    crash fix regarding cyclic recycler with no items

--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/adapter/RecyclerComponentAdapter.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/adapter/RecyclerComponentAdapter.kt
@@ -133,7 +133,7 @@ open class RecyclerComponentAdapter @JvmOverloads constructor(
     override fun onCyclicMiddleIndexUpdated(cyclicMiddleIndex: Int) {
         if (cyclicMiddleIndex > 0) {
             Handler().postDelayed({ scrollImmediately(cyclicMiddleIndex) }, 2000)//TODO change this ugly thing to listen to when the RecyclerView finished inflating the Views
-        } //else - no items - scroll() will crash because position of item doesn't exist
+        } //else - no items
     }
 
 
