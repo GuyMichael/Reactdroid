cec7ab8219459813d6432798ca30794e8c6d4aec
Guy Michael <guy@engieapp.com>
2020-07-02 20:33:43 +0300

    added AComponent.withBtn(id) injection

--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/text/CBtn.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/text/CBtn.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Button
 import androidx.annotation.IdRes
 import com.guymichael.kotlinreact.model.EmptyOwnState
+import com.guymichael.reactdroid.core.model.AComponent
 
 class CBtn(v: Button) : BaseTextComponent<TextProps, EmptyOwnState, Button>(v) {
     override fun createInitialState(props: TextProps) = EmptyOwnState
@@ -12,5 +13,6 @@ class CBtn(v: Button) : BaseTextComponent<TextProps, EmptyOwnState, Button>(v) {
 
 //THINK as Annotations
 fun withBtn(textView: Button) = CBtn(textView)
+fun AComponent<*, *, *>.withBtn(@IdRes id: Int) = CBtn(mView.findViewById(id))
 fun View.withBtn(@IdRes id: Int) = CBtn(findViewById(id))
 fun Activity.withBtn(@IdRes id: Int) = CBtn(findViewById(id))
