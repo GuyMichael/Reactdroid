d1179a801ce550c98f50cfb80956f751719f1648
Guy Michael <guy@engieapp.com>
2020-07-13 01:12:18 +0300

    fixed divider using size as color

--- reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/dividers/ListDivider.kt
+++ reactdroid/src/main/java/com/guymichael/reactdroid/extensions/components/list/dividers/ListDivider.kt
@@ -2,18 +2,16 @@ package com.guymichael.reactdroid.extensions.components.list.dividers
 
 import androidx.annotation.ColorInt
 
-data class ListDivider(val widthPx: Int, val heightPx: Int, @ColorInt val argbColor: Int? = null) {
+data class ListDivider @JvmOverloads constructor(
+    val widthPx: Int
+    , val heightPx: Int = widthPx
+    , @ColorInt val argbColor: Int? = null
+)/* {
 
-    constructor(sizePx: Int)
-    : this(sizePx, sizePx)
-
-    constructor(sizePx: Int, @ColorInt argbColor: Int)
-    : this(sizePx, sizePx, argbColor)
-
-    /*fun toDrawable(): Drawable {
+    fun toDrawable(): Drawable {
         return GradientDrawable().also {
             it.setSize(widthPx, heightPx)
             argbColor?.also(it::setColor)
         }
-    }*/
-}
+    }
+}*/
