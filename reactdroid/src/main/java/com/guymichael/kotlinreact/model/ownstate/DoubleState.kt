3b5dca1584d102d84889d2bd20fcdfc93466f2b4
Guy Michael <guy@engieapp.com>
2021-03-12 00:26:56 +0200

    added easy DoubleState setState

--- reactdroid/src/main/java/com/guymichael/kotlinreact/model/ownstate/DoubleState.kt
+++ reactdroid/src/main/java/com/guymichael/kotlinreact/model/ownstate/DoubleState.kt
@@ -1,9 +1,14 @@
 package com.guymichael.kotlinreact.model.ownstate
 
+import com.guymichael.kotlinreact.model.Component
 import com.guymichael.kotlinreact.model.OwnState
 import java.io.Serializable
 
 /** Simple [Double] state for simple components or component wrappers (HOCs of any kind) */
 data class DoubleState(val value: Double?) : OwnState(), Serializable {
     override fun getAllMembers() = listOf(value)
+}
+
+fun Component<*, DoubleState>.setState(nextValue: Double?) {
+    this.setState(DoubleState(nextValue))
 }
