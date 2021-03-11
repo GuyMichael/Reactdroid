8f94009c5866ee7b3c4de2394c2e81f8c992050c
Guy Michael <guy@engieapp.com>
2021-03-11 16:46:25 +0200

    added injection for easy Boolean setState

--- reactdroid/src/main/java/com/guymichael/kotlinreact/model/ownstate/BooleanState.kt
+++ reactdroid/src/main/java/com/guymichael/kotlinreact/model/ownstate/BooleanState.kt
@@ -1,9 +1,13 @@
 package com.guymichael.kotlinreact.model.ownstate
 
+import com.guymichael.kotlinreact.model.Component
 import com.guymichael.kotlinreact.model.OwnState
 import java.io.Serializable
 
 /** Simple [Boolean] state for simple components or component wrappers (HOCs of any kind) */
 data class BooleanState(val value: Boolean) : OwnState(), Serializable {
     override fun getAllMembers() = listOf(value)
+}
+fun Component<*, BooleanState>.setState(value: Boolean) {
+    this.setState(BooleanState(value))
 }
