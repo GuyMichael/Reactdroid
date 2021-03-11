2eb6009034acfeb5eb41aa372db3d1075e66975a
Guy Michael <guy@engieapp.com>
2021-03-12 00:27:41 +0200

    added easy DoubleProps onRender

--- reactdroid/src/main/java/com/guymichael/kotlinreact/model/props/DoubleProps.kt
+++ reactdroid/src/main/java/com/guymichael/kotlinreact/model/props/DoubleProps.kt
@@ -1,9 +1,14 @@
 package com.guymichael.kotlinreact.model.props
 
+import com.guymichael.kotlinreact.model.Component
 import com.guymichael.kotlinreact.model.OwnProps
 import java.io.Serializable
 
 /** Simple [Double] props for simple components or component wrappers (HOCs of any kind) */
 data class DoubleProps(val value: Double?) : OwnProps(), Serializable {
     override fun getAllMembers() = listOf(value)
+}
+
+fun Component<DoubleProps, *>.onRender(nextValue: Double?) {
+    this.onRender(DoubleProps(nextValue))
 }
